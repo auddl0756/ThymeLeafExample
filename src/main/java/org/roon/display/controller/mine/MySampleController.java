3750243cc8dcc6e866cdadd15a99976a9cfb8ca8
auddl0756 <cjddl789123@naver.com>
2022-01-04 22:38:59 +0900

    static으로 샘플 데이터 만들어 놓도록 변경

--- src/main/java/org/roon/display/controller/mine/MySampleController.java
+++ src/main/java/org/roon/display/controller/mine/MySampleController.java
@@ -6,11 +6,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Controller
 public class MySampleController {
+    private static List<SampleDto> dtoList;
+
+    static{
+        dtoList = IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> {
+                    SampleDto dto = SampleDto.builder()
+                            .id(i)
+                            .name("leemr")
+                            .time(LocalDateTime.now())
+                            .build();
+                    return dto;
+                }).collect(Collectors.toList());
+    }
+
     @GetMapping("/mine/dto")
     public String getDto(Model model){
         SampleDto sampleDto = SampleDto.builder()
@@ -26,16 +42,6 @@ public class MySampleController {
 
     @GetMapping("/mine/dtoList")
     public String getDtoList(Model model){
-        var dtoList = IntStream.rangeClosed(1, 10)
-                .mapToObj(i -> {
-                    SampleDto dto = SampleDto.builder()
-                            .id(i)
-                            .name("leemr")
-                            .time(LocalDateTime.now())
-                            .build();
-                    return dto;
-                }).collect(Collectors.toList());
-
         model.addAttribute("dtoList", dtoList);
 
         return "mine/dtoList";
@@ -53,5 +59,11 @@ public class MySampleController {
         model.addAttribute("dto",dto);
 
         return "mine/inlineJs";
+    }
+
+    @GetMapping("/mine/block")
+    public String block(Model model){
+        model.addAttribute("dtoList",dtoList);
+        return "mine/block";
     }
 }
